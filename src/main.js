44f2224202953e0862c63ef346e76d47ea2c873d
Егор Северов <egorseveroff@gmail.com>
2025-06-14 23:32:45 +0400

    Создал страницу, подключил стили bootstrap

--- src/main.js
+++ src/main.js
@@ -1,25 +1,19 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
 import  './styles.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.getElementById("rss-form");
+const input = document.getElementById("url-input");
 
-setupCounter(document.querySelector('#counter'));
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const url = input.value.trim();
+
+  if (!url) {
+    alert("Пожалуйста, введите ссылку RSS");
+    return;
+  }
+
+  console.log("Добавляем RSS:", url);
+  input.value = "";
+});
