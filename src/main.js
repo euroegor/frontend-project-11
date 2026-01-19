5a9d08d8e0b400846fb08a1d7eb7944d5910d06a
Егор Северов <egorseveroff@gmail.com>
2026-01-20 02:51:30 +0400

    added yup validation with on-change

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-import render from './view.js';
+import render from "./view.js";
+import onChange from "on-change";
+import * as yup from "yup";
 
 const state = {
   form: {
-    status: 'idle', // idle | sending | success | failed
+    status: "idle", // idle | success | failed
     error: null,
+    url: "",
   },
   feeds: [],
   posts: [],
@@ -14,8 +17,45 @@ const state = {
   },
 };
 
+const form = document.getElementById("rss-form");
+//const feedback = document.querySelector('.feedback');
+
+const watchedState = onChange(state, () => {
+  render(watchedState);
+});
+
+const schema = yup
+  .string()
+  .trim()
+  .required("Поле не должно быть пустым")
+  .url("Ссылка должна быть валидным URL")
+  .test("is-duplicate-rss", "RSS уже существует", (url) => {
+    if (watchedState.feeds.some((item) => item.url === url)) {
+      return false;
+    }
+    return true;
+  });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const input = document.getElementById("url-input");
+  watchedState.form.url = input.value.trim();
+  schema
+  .validate(watchedState.form.url)
+  .then(() => {
+    watchedState.form.error = null;
+    watchedState.form.status = 'success';
+    watchedState.feeds.push({ url: watchedState.form.url });
+    watchedState.form.url = '';
+  })
+  .catch((err) => {
+    watchedState.form.error = err.message;
+    watchedState.form.status = 'failed';
+  })
+});
+
 const init = () => {
-  render(state);
+  render(watchedState);
 };
 
 init();
